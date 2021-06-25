c092840fc28424558adf6ce10a55a50a7f611e5c
July Mariana <julymarianalima@gmail.com>
2021-06-25 00:28:57 -0300

    a simple example with spark and scala
    using ml-100k dataset to test some scala features

---
+++ popular_movies.scala
@@ -0,0 +1,40 @@
+package app.PopularMoviesDataset
+
+import org.apache.log4j._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, LongType, StructType}
+
+
+object scala {
+  final case class Movie(movieID: Int)
+
+  def main(args: Array[String]): Unit ={
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark = SparkSession
+      .builder()
+      .appName("PopularMovies")
+      .master("local[*]")
+      .getOrCreate()
+
+    val moviesSchema = new StructType()
+      .add("userID", IntegerType, nullable = true)
+      .add("movieID",IntegerType,nullable = true)
+      .add("rating",IntegerType, nullable = true)
+      .add("timestamp", LongType, nullable =true)
+
+    import spark.implicits._
+    val moviesDS = spark.read
+      .option("sep","\t")
+      .schema(moviesSchema)
+      .csv("C:\\Users\\<user>\\Documents\\ml-100k")
+      .as[Movie]
+
+    val topMoviesIDs = moviesDS.groupBy("movieID").count().orderBy("count")
+
+    topMoviesIDs.show(10)
+
+    spark.stop()
+
+  }
+}
